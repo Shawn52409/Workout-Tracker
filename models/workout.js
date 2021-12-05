26feb53818dee3b589089249f1793c23ae7d7437
Shawn Miller <shawnsmm@gmail.com>
2021-12-04 19:35:51 -0500

    Created workout model

---
+++ models/workout.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Schema for new workout
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: {
+    type: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    duration: {
+        type: Number,
+    },
+    weight: {
+        type: Number,
+    },
+    reps: {
+        type: Number,
+    },
+    distance: {
+        type: Number,
+    },
+    sets: {
+        type: Number,
+    },
+  },
+});
+
+// Create workout db model with workout schema
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
