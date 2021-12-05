42b8119eaa33f548ddbd110e5840a2dba969f14d
Shawn Miller <shawnsmm@gmail.com>
2021-12-04 22:01:51 -0500

    Created api route starter code

---
+++ routes/api-route.js
@@ -0,0 +1,26 @@
+// Required dependancies
+const router = require("express").Router();
+const Workout = require("../models");
+
+// Get the last work
+router.get("api/workouts", (req, res) => {
+
+});
+
+// Get the workout within one week
+router.get("api/workouts/range", (req, res) => {
+
+});
+
+// Add a workout
+router.put("api/workouts", (req, res) => {
+
+});
+
+// Create new workout
+router.post("api/workouts", (req, res) => {
+
+});
+
+// Export routes
+module.exports = router;
